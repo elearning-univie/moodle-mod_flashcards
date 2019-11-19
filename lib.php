2046224377478faf30ed2aec0173e45aa977fc21
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2019-11-19 08:26:47 +0100

    fix name

--- lib.php
+++ lib.php
@@ -3,11 +3,23 @@ function flashcards_add_instance($flashcards) {
     global $DB;
     $object = new stdClass();
     $object->timecreated = time();
-    if (property_exists($flashcards, 'intro') || $flashcards->intro == null) {
-        $object->intro = '';
+    if (property_exists($flashcards, 'intro') || $flashcards -> intro == null) {
+        $flashcards -> intro = '';
     } else {
-        $object->intro = $flashcards;
+        $flashcards -> intro = $flashcards;
     }
+
+    $catids = explode(",", $flashcards->category);
+
+    if ($flashcards->newcategory) {
+        $newcat = new stdClass();
+        $newcat->name = $flashcards->newcategoryname;
+        $newcat->contextid = $catids[1];
+        $newcat->info = 'Created via Flashcard Activity';
+        $qcid = $DB->insert_record('question_categories', $newcat);
+        $flashcards->categoryid = $qcid;
+    }
+
     $object = $DB->insert_record('mod_flashcards', $object);
     return;
 }
--- mod_form.php
+++ mod_form.php
@@ -7,12 +7,35 @@ require_once($CFG->dirroot.'/course/moodleform_mod.php');
 require_once($CFG->dirroot.'/mod/flashcards/lib.php');
 
 class mod_flashcards_mod_form extends moodleform_mod {
+
     function definition() {
         $mform =& $this->_form;
 
         $mform->addElement('text', 'name', get_string('flashcardname', 'flashcards'), array('size'=>'64'));
         $mform->setType('name', PARAM_TEXT);
         $mform->addRule('name', null, 'required', null, 'client');
+
+        $options = array(
+            1 => FLASHCARDS_NEW,
+            0 => FLASHCARDS_EXISTING
+        );
+
+        // Introduction.
+        $this->standard_intro_elements();
+
+        $mform->addElement('select', 'newcategory', get_string('newexistingcategory', 'flashcards'), $options);
+        $mform->setType('newcategory', PARAM_INT);
+        $mform->setDefault('newcategory', 1);
+
+        $mform->addElement('text', 'newcategoryname', get_string('newcategoryname','flashcards'),  array('size'=>'64'));
+        $mform->setType('newcategoryname', PARAM_TEXT);
+        $mform->hideIf('newcategoryname', 'neworexistingcategory', 'eq','existing');
+        
+        
+        $mform->addElement('questioncategory', 'category', get_string('category', 'question'), array('contexts' => $context));
+
+        $mform->addElement('checkbox', 'includesubcategories', get_string('includesubcategories', 'flashcards'));
+        $mform->hideIf('includesubcategories', 'neworexistingcategory', 'eq', 'new');
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
     }
