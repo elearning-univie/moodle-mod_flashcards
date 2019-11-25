d7a3e3ed4ab3ac509cbed2a671a1c697294678f7
Angela Baier <angela.baier@univie.ac.at>
2019-11-25 16:22:57 +0100

    EL-117, split view in student and teacher

--- teacherview.php
+++ teacherview.php
@@ -4,10 +4,12 @@ require_once('lib.php');
 
 global $PAGE, $OUTPUT, $COURSE, $USER;
 
+//$context = context_course::instance($COURSE->id);
+
 $id = required_param('id', PARAM_INT);
 list ($course, $cm) = get_course_and_cm_from_cmid($id, 'flashcards');
 
-$context = context_module::instance($cm->id);
+$context = get_context_instance(CONTEXT_MODULE, $cm->id);
 
 require_login($course, false, $cm);
 
@@ -23,17 +25,9 @@ $pagetitle = get_string('pagetitle', 'flashcards');
 $PAGE->set_title($pagetitle);
 $PAGE->set_heading($course->fullname);
 
-if (has_capability('mod/flashcards:studentview', $context) ) {
-    $redirecturl = new moodle_url('/mod/flashcards/studentview.php', array('id' => $id));
-    redirect($redirecturl);}
-if (has_capability('mod/flashcards:teacherview', $context) ) {
-    $redirecturl = new moodle_url('/mod/flashcards/teacherview.php', array('id' => $id));
-    redirect($redirecturl);
-} else {
-    print("hier kommt die Auswahl für beide hin.");
-}
-
 echo $OUTPUT->header();
 echo $OUTPUT->heading($flashcards->name);
+
+print("THIS IS TEACHER VIEW.");
 
 echo $OUTPUT->footer();
