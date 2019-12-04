14ccfa70749d890d7b2a7273a04dcf96ea846f64
Adrian Czermak <adrian.czermak@univie.ac.at>
2019-12-04 17:16:01 +0100

    dev2019120401

--- amd/src/studentcontroller.js
+++ amd/src/studentcontroller.js
@@ -1,10 +1,12 @@
 define(['jquery', 'core/ajax', 'core/notification'], function ($, ajax, notification){
     return {
         init: function() {
-            $.mod_flashcards_call_update = function () {
+            $.mod_flashcards_call_update = function ($courseid, $questionid) {
+                window.console.log($courseid);
+                window.console.log($questionid);
                 ajax.call([{
                     methodname: 'mod_flashcards_update_progress',
-                    args: {courseid: 1},
+                    args: {courseid: $courseid, questionid: $questionid},
                     done: window.console.log("ajax done"),
                     fail: notification.exception
                 }]);
--- externallib.php
+++ externallib.php
@@ -11,17 +11,18 @@ class mod_flashcards_external extends external_api {
     public static function update_progress_parameters() {
         return new external_function_parameters(
             array(
-                    'courseid' => new external_value(PARAM_INT, 'id of course')
+                    'courseid' => new external_value(PARAM_INT, 'id of course'),
+                    'questionid' => new external_value(PARAM_INT, 'id of course')
             )
         );
     }
 
-    public static function update_progress($courseid) {
+    public static function update_progress($courseid, $questionid) {
         global $DB;
-        return 2;
+        return 1;
     }
 
     public static function update_progress_returns() {
-        return new external_value(PARAM_INT, 'id of course');
+        return new external_value(PARAM_INT, '1 if success, 0 if failed');
     }
 }
