64b873ec89470b65937ea354ebfa5da0ee1990a6
Adrian Czermak <adrian.czermak@univie.ac.at>
2020-02-11 15:18:21 +0100

    bugfix

--- amd/src/questioninit.js
+++ amd/src/questioninit.js
@@ -3,20 +3,22 @@ define(['jquery', 'core/ajax', 'core/notification', 'core/url'], function ($, aj
         init: function () {
             $.mod_flashcards_load_questions = function (aid, cmid) {
                 let data = document.querySelectorAll(".mod-flashcards-checkbox");
-                var qids = {};
+                var qids = [];
                 for (let i = 0; i < data.length; i++) {
                     if (data[i].checked == true) {
                         qids[i] = data[i].dataset.value;
                     }
                 }
-                ajax.call([{
-                    methodname: 'mod_flashcards_load_questions',
-                    args: {flashcardsid: aid, qids: qids},
-                    done: function () {
-                        window.location = url.relativeUrl('/mod/flashcards/studentview.php?id=' + cmid);
-                    },
-                    fail: notification.exception
-                }]);
+                if (qids && qids.length) {
+                    ajax.call([{
+                        methodname: 'mod_flashcards_load_questions',
+                        args: {flashcardsid: aid, qids: qids},
+                        done: function () {
+                            window.location = url.relativeUrl('/mod/flashcards/studentview.php?id=' + cmid);
+                        },
+                        fail: notification.exception
+                    }]);
+                }
             };
             $.mod_flashcards_select_all = function (selected) {
                 $('input:checkbox').not(selected).prop('checked', selected.checked);
