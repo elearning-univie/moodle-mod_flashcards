24407dcf38dd8ec974b37c20523a0e583e047e8d
Adrian Czermak <adrian.czermak@univie.ac.at>
2020-12-09 18:58:39 +0100

    removed form fields

--- studentcreatequestion.php
+++ studentcreatequestion.php
@@ -27,7 +27,7 @@ require_once($CFG->dirroot . '/question/editlib.php');
 require_once($CFG->libdir . '/filelib.php');
 require_once($CFG->libdir . '/formslib.php');
 
-global $USER, $DB, $PAGE, $COURSE;
+global $USER, $DB, $PAGE, $COURSE, $OUTPUT;
 
 // Read URL parameters telling us which question to edit.
 $id = optional_param('id', 0, PARAM_INT); // question id
@@ -35,8 +35,6 @@ $makecopy = optional_param('makecopy', 0, PARAM_BOOL);
 $qtype = 'flashcard';
 $categoryid = optional_param('category', 0, PARAM_INT);
 $cmid = required_param('cmid', PARAM_INT);
-$wizardnow = optional_param('wizardnow', '', PARAM_ALPHA);
-$originalreturnurl = optional_param('returnurl', 0, PARAM_LOCALURL);
 $appendqnumstring = optional_param('appendqnumstring', '', PARAM_ALPHA);
 $inpopup = optional_param('inpopup', 0, PARAM_BOOL);
 $scrollpos = optional_param('scrollpos', 0, PARAM_INT);
@@ -57,12 +55,6 @@ if ($categoryid !== 0) {
 if ($cmid !== 0) {
     $url->param('cmid', $cmid);
 }
-if ($wizardnow !== '') {
-    $url->param('wizardnow', $wizardnow);
-}
-if ($originalreturnurl !== 0) {
-    $url->param('returnurl', $originalreturnurl);
-}
 if ($appendqnumstring !== '') {
     $url->param('appendqnumstring', $appendqnumstring);
 }
@@ -78,14 +70,7 @@ $questionbankurl = new moodle_url('/question/edit.php', array('cmid' => $cmid));
 
 navigation_node::override_active_url($questionbankurl);
 
-if ($originalreturnurl) {
-    if (strpos($originalreturnurl, '/') !== 0) {
-        throw new coding_exception("returnurl must be a local URL starting with '/'. $originalreturnurl was given.");
-    }
-    $returnurl = new moodle_url($originalreturnurl);
-} else {
-    $returnurl = $questionbankurl;
-}
+$returnurl = $questionbankurl;
 if ($scrollpos) {
     $returnurl->param('scrollpos', $scrollpos);
 }
@@ -95,7 +80,7 @@ require_login($cm->course, false, $cm);
 $thiscontext = context_module::instance($cmid);
 
 $contexts = new question_edit_contexts($thiscontext);
-$PAGE->set_pagelayout('admin');
+//$PAGE->set_pagelayout('admin');
 
 if (optional_param('addcancel', false, PARAM_BOOL)) {
     redirect($returnurl);
@@ -105,9 +90,6 @@ if ($id) {
     if (!$question = $DB->get_record('question', array('id' => $id))) {
         print_error('questiondoesnotexist', 'question', $returnurl);
     }
-    // We can use $COURSE here because it's been initialised as part of the
-    // require_login above. Passing it as the third parameter tells the function
-    // to filter the course tags by that course.
     get_question_options($question, true, [$COURSE]);
 
 } else if ($categoryid && $qtype) { // only for creating new questions
@@ -116,14 +98,11 @@ if ($id) {
     $question->qtype = $qtype;
     $question->createdby = $USER->id;
 
-    // Check that users are allowed to create this question type at the moment.
     if (!question_bank::qtype_enabled($qtype)) {
         print_error('cannotenable', 'question', $returnurl, $qtype);
     }
 
 } else if ($categoryid) {
-    // Category, but no qtype. They probably came from the addquestion.php
-    // script without choosing a question type. Send them back.
     $addurl = new moodle_url('/question/addquestion.php', $url->params());
     $addurl->param('validationerror', 1);
     redirect($addurl);
@@ -137,13 +116,11 @@ $qtypeobj = question_bank::get_qtype($question->qtype);
 if (isset($question->categoryobject)) {
     $category = $question->categoryobject;
 } else {
-    // Validate the question category.
     if (!$category = $DB->get_record('question_categories', array('id' => $question->category))) {
         print_error('categorydoesnotexist', 'question', $returnurl);
     }
 }
 
-// Check permissions
 $question->formoptions = new stdClass();
 
 $categorycontext = context::instance_by_id($category->contextid);
@@ -161,13 +138,12 @@ if ($id) {
         question_require_capability_on($question, 'view');
     }
     if ($makecopy) {
-        // If we are duplicating a question, add some indication to the question name.
         $question->name = get_string('questionnamecopy', 'question', $question->name);
         $question->idnumber = core_question_find_next_unused_idnumber($question->idnumber, $category->id);
         $question->beingcopied = true;
     }
 
-} else  { // creating a new question
+} else  {
     $question->formoptions->canedit = question_has_capability_on($question, 'edit');
     $question->formoptions->canmove = (question_has_capability_on($question, 'move') && $addpermission);
     $question->formoptions->cansaveasnew = false;
@@ -177,17 +153,15 @@ if ($id) {
 }
 $question->formoptions->mustbeusable = (bool) $appendqnumstring;
 
-// Validate the question type.
 $PAGE->set_pagetype('question-type-' . $question->qtype);
 
-// Create the question editing form.
-if ($wizardnow !== '') {
-    $mform = $qtypeobj->next_wizard_form('question.php', $question, $wizardnow, $formeditable);
-} else {
-    $mform = $qtypeobj->create_editing_form('question.php', $question, $category, $contexts, $formeditable);
-}
-// send the question object and a few more parameters to the form
-$toform = fullclone($question);
+$mform = $qtypeobj->create_editing_form('question.php', $question, $category, $contexts, $formeditable);
+$mform->removeFormElement('category');
+// TODO set category automatically
+$mform->removeFormElement('generalfeedback');
+$mform->removeFormElement('idnumber');
+
+/*$toform = fullclone($question);
 $toform->category = "{$category->id},{$category->contextid}";
 $toform->scrollpos = $scrollpos;
 if ($formeditable && $id){
@@ -206,7 +180,7 @@ if ($cm !== null){
 
 $toform->inpopup = $inpopup;
 
-$mform->set_data($toform);
+$mform->set_data($toform);*/
 
 if ($mform->is_cancelled()) {
     if ($inpopup) {
@@ -216,14 +190,11 @@ if ($mform->is_cancelled()) {
     }
 
 } else if ($fromform = $mform->get_data()) {
-    // If we are saving as a copy, break the connection to the old question.
     if ($makecopy) {
         $question->id = 0;
         $question->hidden = 0; // Copies should not be hidden.
     }
 
-    // Process the combination of usecurrentcat, categorymoveto and category form
-    // fields, so the save_question method only has to consider $fromform->category
     if (!empty($fromform->usecurrentcat)) {
         // $fromform->category is the right category to save in.
     } else {
@@ -234,8 +205,6 @@ if ($mform->is_cancelled()) {
         }
     }
 
-    // If we are moving a question, check we have permission to move it from
-    // whence it came. (Where we are moving to is validated by the form.)
     list($newcatid, $newcontextid) = explode(',', $fromform->category);
     if (!empty($question->id) && $newcatid != $question->category) {
         $contextid = $newcontextid;
@@ -244,10 +213,8 @@ if ($mform->is_cancelled()) {
         $contextid = $category->contextid;
     }
 
-    // Ensure we redirect back to the category the question is being saved into.
     $returnurl->param('category', $fromform->category);
 
-    // We are actually saving the question.
     if (!empty($question->id)) {
         question_require_capability_on($question, 'edit');
     } else {
@@ -259,21 +226,17 @@ if ($mform->is_cancelled()) {
 
     $question = $qtypeobj->save_question($question, $fromform);
     if (isset($fromform->tags)) {
-        // If we have any question context level tags then set those tags now.
         core_tag_tag::set_item_tags('core_question', 'question', $question->id,
                 context::instance_by_id($contextid), $fromform->tags, 0);
     }
 
     if (isset($fromform->coursetags)) {
-        // If we have and course context level tags then set those now.
         core_tag_tag::set_item_tags('core_question', 'question', $question->id,
                 context_course::instance($fromform->courseid), $fromform->coursetags, 0);
     }
 
-    // Purge this question from the cache.
     question_bank::notify_question_edited($question->id);
 
-    // If we are saving and continuing to edit the question.
     if (!empty($fromform->updatebutton)) {
         $url->param('id', $question->id);
         $url->remove_params('makecopy');
@@ -300,7 +263,6 @@ if ($mform->is_cancelled()) {
                 'appendqnumstring' => $appendqnumstring,
                 'scrollpos' => $scrollpos);
         if (isset($fromform->nextpageparam) && is_array($fromform->nextpageparam)){
-            //useful for passing data to the next page which is not saved in the database.
             $nexturlparams += $fromform->nextpageparam;
         }
         $nexturlparams['id'] = $question->id;
@@ -322,5 +284,5 @@ $PAGE->set_heading($COURSE->fullname);
 $PAGE->navbar->add($streditingquestion);
 
 echo $OUTPUT->header();
-$qtypeobj->display_question_editing_page($mform, $question, $wizardnow);
+$qtypeobj->display_question_editing_page($mform, $question, null);
 echo $OUTPUT->footer();
