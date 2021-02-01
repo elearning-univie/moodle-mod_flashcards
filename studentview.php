2a5cc4f64e14071d13091676a7d007c04679f8f1
Adrian Czermak <adrian.czermak@univie.ac.at>
2021-02-01 16:04:51 +0100

    behat fix

--- studentview.php
+++ studentview.php
@@ -106,7 +106,7 @@ if (has_capability('mod/flashcards:studentview', $context)) {
     $templatestablecontext['flashcardsid'] = $flashcards->id;
 
     $templatestablecontext['createfcurl'] = new moodle_url('/mod/flashcards/simplequestion.php',
-            ['cmid' => $cm->id, 'origin' => new moodle_url('/mod/flashcards/studentquestioninit.php', ['cmid' => $id])]);
+            ['cmid' => $cm->id, 'origin' => $templatestablecontext['selectquestionsurl']]);
 
     $renderer = $PAGE->get_renderer('core');
     echo $renderer->render_from_template('mod_flashcards/studentview', $templatestablecontext);
