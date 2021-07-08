ed9616e9f4a853b6b2c2530acc8aec07465c0a2a
Angela Baier <angela.baier@univie.ac.at>
2021-07-08 10:07:46 +0200

    teachercheck reset EL-976

--- db/events.php
+++ db/events.php
@@ -15,21 +15,17 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Defines the version and other meta-info about the plugin
+ * Mod flashcards events.
  *
- * Setting the $plugin->version to 0 prevents the plugin from being installed.
- * See https://docs.moodle.org/dev/version.php for more info.
- *
- * @package   mod_flashcards
- * @copyright 2020 University of Vienna
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    mod_flashcards
+ * @copyright  2021 University of vienna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'mod_flashcards';
-$plugin->version = 2021062900.01;
-$plugin->release = 'v0.0.1';
-$plugin->requires = 2020061501;
-$plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array('qtype_flashcard' => 2020111300);
+$observers = array(
+    
+    array(
+        'eventname'   => '\core\event\question_updated',
+        'callback'    => 'mod_flashcards_observer::question_updated',
+    ),
+);
