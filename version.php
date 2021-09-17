0a3a69ab009ea0a28f878b913dbc0171732ba560
Angela Baier <angela.baier@univie.ac.at>
2021-09-17 12:47:47 +0200

    version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_flashcards';
-$plugin->version = 2021091400;
+$plugin->version = 2021091401;
 $plugin->release = 'v0.0.1';
 $plugin->requires = 2020061501;
 $plugin->maturity = MATURITY_STABLE;
