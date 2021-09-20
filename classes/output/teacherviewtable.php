11de81a2168425cdba6f90c3c3717814b0b7cf27
Angela Baier <angela.baier@univie.ac.at>
2021-09-20 11:13:40 +0200

    pipeline

--- classes/output/teacherviewtable.php
+++ classes/output/teacherviewtable.php
@@ -92,7 +92,7 @@ class teacherviewtable extends table_sql {
         $this->editicontext = get_string('edit', 'moodle');
         $this->deleteicontext = get_string('delete', 'moodle');
         $this->previewicontext = get_string('fcview', 'mod_flashcards');
-        
+
         $thumbsup = '<i class="icon fa fa-thumbs-up fa-fw " title="Yes" aria-label="Yes" style="color:green;"></i>';
         $thumbsdown = '<i class="icon fa fa-thumbs-down fa-fw " title="No" aria-label="No" style="color:red;"></i>';
 
