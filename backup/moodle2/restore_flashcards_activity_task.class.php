02801af95a44ece6806f79ed06e25a98b6742f53
Angela Baier <angela.baier@univie.ac.at>
2021-10-18 11:02:08 +0200

    button fix

--- backup/moodle2/restore_flashcards_activity_task.class.php
+++ backup/moodle2/restore_flashcards_activity_task.class.php
@@ -57,7 +57,7 @@ class restore_flashcards_activity_task extends restore_activity_task {
      * Define the contents in the activity that must be
      * processed by the link decoder
      */
-    static public function define_decode_contents() {
+    public static function define_decode_contents() {
         $contents = array();
 
         $contents[] = new restore_decode_content('flashcards', array('intro'), 'flashcards');
@@ -69,7 +69,7 @@ class restore_flashcards_activity_task extends restore_activity_task {
      * Define the decoding rules for links belonging
      * to the activity to be executed by the link decoder
      */
-    static public function define_decode_rules() {
+    public static function define_decode_rules() {
         $rules = array();
 
         $rules[] = new restore_decode_rule('FLASHCARDSVIEWBYID', '/mod/flashcards/view.php?id=$1', 'course_module');
@@ -85,7 +85,7 @@ class restore_flashcards_activity_task extends restore_activity_task {
      * flashcards logs. It must return one array
      * of {@see restore_log_rule} objects
      */
-    static public function define_restore_log_rules() {
+    public static function define_restore_log_rules() {
         $rules = array();
 
         $rules[] = new restore_log_rule('flashcards', 'add', 'view.php?id={course_module}', '{flashcards}');
@@ -105,7 +105,7 @@ class restore_flashcards_activity_task extends restore_activity_task {
      * by the restore final task, but are defined here at
      * activity level. All them are rules not linked to any module instance (cmid = 0)
      */
-    static public function define_restore_log_rules_for_course() {
+    public static function define_restore_log_rules_for_course() {
         $rules = array();
 
         $rules[] = new restore_log_rule('flashcards', 'view all', 'index.php?id={course}', null);
