034f6042751722af38e3380bb8498d5b038ab80e
Jakob Mischke <jakob.mischke@univie.ac.at>
2021-11-05 12:32:02 +0000

    El 405 import export

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_flashcards';
-$plugin->version = 2021110200;
+$plugin->version = 2021110500;
 $plugin->release = 'v0.0.1';
 $plugin->requires = 2020061501;
 $plugin->maturity = MATURITY_STABLE;
